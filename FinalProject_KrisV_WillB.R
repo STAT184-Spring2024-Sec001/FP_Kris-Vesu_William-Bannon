0b55fbc1465905f255e5d3f0353470ab3b304677
krisvesu <166137043+krisvesu@users.noreply.github.com>
2024-04-22 13:05:33 -0400

    Update FinalProject_KrisV_WillB.R

--- FinalProject_KrisV_WillB.R
+++ FinalProject_KrisV_WillB.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 lebron_data <- read.csv("C:/Users/krisv/Downloads/career_data/lebron_career.csv")
 lebron_data$age <- substr(lebron_data$age, 1, 2)
 write.csv(lebron_data, "modified_lebron_data.csv", row.names = FALSE)

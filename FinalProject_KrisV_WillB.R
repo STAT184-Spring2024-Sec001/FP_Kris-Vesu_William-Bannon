dd217932eb80d412579fd40444d5561a3b669ebb
krisvesu <166137043+krisvesu@users.noreply.github.com>
2024-04-22 12:58:21 -0400

    Update FinalProject_KrisV_WillB.R

--- FinalProject_KrisV_WillB.R
+++ FinalProject_KrisV_WillB.R
@@ -1,5 +1,9 @@
 lebron_data <- read.csv("C:/Users/krisv/Downloads/career_data/lebron_career.csv")
+lebron_data$age <- substr(lebron_data$age, 1, 2)
+write.csv(lebron_data, "modified_lebron_data.csv", row.names = FALSE)
 View(lebron_data)
 
 jordan_data <- read.csv("C:/Users/krisv/Downloads/career_data/jordan_career.csv")
+jordan_data$age <- substr(jordan_data$age, 1, 2)
+write.csv(jordan_data, "modified_jordan_data.csv", row.names = FALSE)
 View(jordan_data)

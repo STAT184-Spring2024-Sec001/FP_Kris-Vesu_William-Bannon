a1236791f116075d51cac7ce2de46d268a9c0d25
willb1203 <166156094+willb1203@users.noreply.github.com>
2024-04-22 13:04:03 -0400

    Update FinalProject_KrisV_WillB.R

--- FinalProject_KrisV_WillB.R
+++ FinalProject_KrisV_WillB.R
@@ -7,3 +7,10 @@ jordan_data <- read.csv("C:/Users/krisv/Downloads/career_data/jordan_career.csv"
 jordan_data$age <- substr(jordan_data$age, 1, 2)
 write.csv(jordan_data, "modified_jordan_data.csv", row.names = FALSE)
 View(jordan_data)
+
+ggplot() +
+  geom_line(data = lebron_data, aes(x = lebron_data$age, y = lebron_data$pts), color = "blue") +
+  geom_line(data = jordan_data, aes(x = lebron_data$age, y = jordan_data$pts), color = "red") +
+  labs(title = "Points by each GOAT",
+       x = "Age",
+       y = "Points per Game")

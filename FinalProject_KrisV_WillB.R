4511dd4e087b24701f78f98c6752df9df2bc2715
krisvesu <166137043+krisvesu@users.noreply.github.com>
2024-04-22 12:47:58 -0400

    Add files via upload

---
+++ FinalProject_KrisV_WillB.R
@@ -0,0 +1,5 @@
+lebron_data <- read.csv("C:/Users/krisv/Downloads/career_data/lebron_career.csv")
+View(lebron_data)
+
+jordan_data <- read.csv("C:/Users/krisv/Downloads/career_data/jordan_career.csv")
+View(jordan_data)
